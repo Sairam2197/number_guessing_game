1c0bfbc1b43f5924f01c5c504eacde6a6381cfe0
Sairam <mstrthedespicable@gmail.com>
2023-06-03 16:20:36 +0530

    initial

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import random
 import math
 
-
+# getting inputs
 lower = int(input("Enter a lower limit:"))
 upper = int(input("Enter an upper limit:"))
 
+# creating a random number with Random function and making
+# python to produce number of chances available
 x = random.randint(lower,upper)
 chances = round(math.log(upper - lower +1, 2))
 print(f'You have got {chances} changes to guess the number')
 
 count = 0
 
+# Actual game where we guess numbers
 while count < chances:
     count = count + 1
     guess = int(input("Enter a number to guess:"))
@@ -21,6 +24,8 @@ while count < chances:
         print("You guessed too small")
     elif x < guess:
         print("You guess too big")
+
+# If user couldnt guess within the limit, the game ends
 if count >= chances and x != guess:
     print(f'The number is {x}' '\n Better luck next time')
 
